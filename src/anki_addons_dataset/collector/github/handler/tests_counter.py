5c6eb7ab21104f2e1cad122ba788f45941142c06
Aleksey Yablokov <alex_ya@mailbox.org>
2025-07-05 10:33:11 +0700

    Simplify regexp in TestsCounter to fix Sonar warnings

--- src/anki_addons_dataset/collector/github/handler/tests_counter.py
+++ src/anki_addons_dataset/collector/github/handler/tests_counter.py
@@ -1,15 +1,16 @@
 import re
+from pathlib import Path
 
 
 class TestsCounter:
 
     @staticmethod
     def count_tests(files: list[str]) -> int:
-        count: int = len([file for file in files if TestsCounter.__is_test(file)])
+        count: int = len([file for file in files if TestsCounter.__is_test(Path(file))])
         return count
 
     @staticmethod
-    def __is_test(file: str) -> bool:
+    def __is_test(file: Path) -> bool:
         return TestsCounter.__is_python_test(file) or \
             TestsCounter.__is_javascript_test(file) or \
             TestsCounter.__is_rust_test(file) or \
@@ -17,21 +18,21 @@ class TestsCounter:
             TestsCounter.__is_c_test(file)
 
     @staticmethod
-    def __is_python_test(file: str) -> bool:
-        return file.endswith("_test.py") or re.match(r"^\w*/?test_\w+.py", file)
+    def __is_python_test(file: Path) -> bool:
+        return file.name.endswith("_test.py") or re.match(r"test_\w+.py", file.name)
 
     @staticmethod
-    def __is_javascript_test(file: str) -> bool:
-        return file.endswith(".test.js") or file.endswith(".spec.js") or file.endswith(".spec.ts")
+    def __is_javascript_test(file: Path) -> bool:
+        return file.name.endswith(".test.js") or file.name.endswith(".spec.js") or file.name.endswith(".spec.ts")
 
     @staticmethod
-    def __is_typescript_test(file: str) -> bool:
-        return file.endswith(".test.ts") or file.endswith(".spec.ts")
+    def __is_typescript_test(file: Path) -> bool:
+        return file.name.endswith(".test.ts") or file.name.endswith(".spec.ts")
 
     @staticmethod
-    def __is_rust_test(file: str) -> bool:
-        return file.endswith("_test.rs")
+    def __is_rust_test(file: Path) -> bool:
+        return file.name.endswith("_test.rs")
 
     @staticmethod
-    def __is_c_test(file: str) -> bool:
-        return file.endswith("_test.c") or re.match(r"^\w*/?test_\w+.c", file)
+    def __is_c_test(file: Path) -> bool:
+        return file.name.endswith("_test.c") or re.match(r"test_\w+.c", file.name)
