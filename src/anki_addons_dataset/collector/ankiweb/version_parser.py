5494a55c45be6c0b420134d0bd5ca57ede40a183
Aleksey Yablokov <alex_ya@mailbox.org>
2026-02-21 11:39:22 +0800

    Fix Sonar warning "Make sure the regex used here, which is vulnerable to polynomial runtime due to backtracking, cannot lead to denial of service"

--- src/anki_addons_dataset/collector/ankiweb/version_parser.py
+++ src/anki_addons_dataset/collector/ankiweb/version_parser.py
@@ -2,31 +2,25 @@ import datetime
 import re
 from datetime import date
 from re import Match
-from typing import Optional
 
 from anki_addons_dataset.common.data_types import Version
 
 
 class VersionParser:
+    __version_re: re.Pattern[str] = re.compile(
+        r'^(?P<min>\d+(?:\.\d+)*)'
+        r'(?:-(?P<max>\d+(?:\.\d+)*\+?)|(?P<plus>\+))?'
+        r' \(Updated (?P<updated>\d{4}-\d{2}-\d{2})\)$'
+    )
 
     @staticmethod
     def extract_version(version_str: str) -> Version:
-        pattern: str = r'([\d.]+)-?([\d.+]+)? \(Updated (\d{4}-\d{2}-\d{2})\)'
-        match: Optional[Match[str]] = re.search(pattern, version_str)
-        if match:
-            group_count: int = len(match.groups())
-            if group_count == 3:
-                min_version: str = match.group(1)
-                max_version: str = match.group(2)
-                update_date_str: str = match.group(3)
-            elif group_count == 2:
-                min_version: str = match.group(1)
-                max_version: str = ""
-                update_date_str: str = match.group(2)
-            else:
-                raise RuntimeError(f'Cannot parse version: "{version_str}"')
-            update_date: date = datetime.datetime.fromisoformat(update_date_str).date()
-            version_info: Version = Version(min_version, max_version, update_date)
-            return version_info
-        else:
-            raise RuntimeError(f'Cannot parse version: "{version_str}"')
+        match: Match[str] = VersionParser.__version_re.fullmatch(version_str)
+        if not match:
+            raise ValueError(f"Cannot parse version string: {version_str!r}")
+        min_version: str = match.group("min")
+        max_version: str = match.group("max")
+        if max_version is None and match.group("plus") is not None:
+            max_version = "+"
+        updated: date = datetime.date.fromisoformat(match.group("updated"))
+        return Version(min_version=min_version, max_version=max_version, updated=updated)
